f98b88d41902ddf474dad30d6121f1db04aa4b34
Muhammad Moiz Siddique <muhammadmoiz0087@gmail.com>
2021-05-27 14:11:41 +0500

    added material and form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomMaterialDesignModule } from './external-modules/custom-material-design.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    CustomMaterialDesignModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
